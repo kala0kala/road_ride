8b6a72ab3115a6265b0ae372b09d67a4d520a292
kala0kala <50321842+kala0kala@users.noreply.github.com>
2020-11-20 15:24:17 +0100

    Update road_ride.py

--- road_ride.py
+++ road_ride.py
@@ -21,7 +21,7 @@ pygame.init()
 gameDisplay = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE)) #generuje okno
 clock = pygame.time.Clock() #dodaje czas
 roads = pygame.image.load('r1.png') #ładuje drogę do gry
-car = pygame.image.load('cars\8.png') #ładuje auto do gry
+car = pygame.image.load('8.png') #ładuje auto do gry
 bush = pygame.image.load('01.png')
 
 
